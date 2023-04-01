64c0213faf44ff4cf90d38fff9918df3d3f877e6
Hamid Iqbal <hamidiqbal598@gmail.com>
2023-04-01 16:11:06 +0200

    frontend added in react

---
+++ frontend_graphql_blog/src/components/App.js
@@ -0,0 +1,35 @@
+import logo from '../logo.svg';
+import React from 'react';
+import Users from './Users'
+import User from './User'
+
+
+class App extends React.Component {
+  state = {
+    selectedUser: null
+  }
+  selectUser = (user) => {
+    this.setState({selectedUser: user})
+  }
+
+  render() {
+    return(
+        <div className="container mx-auto px-4">
+          {this.state.selectedUser ?
+            <User user={this.state.selectedUser} selectUser={this.selectUser} /> : <Users selectUser={this.selectUser} />
+          }
+        </div>
+    )
+  }
+
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ frontend_graphql_blog/src/components/Users.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import UserImage from './UserImage';
+// import CreateUser from './CreateUser';
+
+const GET_USERS = gql`
+    {
+        users {
+            id
+            name
+            email
+            postsCount
+        }
+    }
+`;
+
+function Users({ selectUser }) {
+    const {loading, error, data} = useQuery(GET_USERS);
+
+    if (loading) return 'Loading....';
+    if (error) return `Error ${error.message}`;
+
+    return (
+        <div className="flex flex-wrap items-center pb-16">
+            {data.users.map(user => (
+                <div key={user.id} className="lg:w-1/3 w-full p-4 text-center inline"
+                     oncClick={selectUser.bind(this, user)}>
+                    <UserImage user={user}/>
+                </div>
+            ))
+            }
+        </div>
+    )
+}
+export default Users;
